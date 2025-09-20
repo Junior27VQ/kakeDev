16c48591bde17e29ce4d3dac15b14d46d611e080
Junior27VQ <favq1999@outlook.com>
2025-09-19 22:02:56 -0500

    se implemento recuperarInt y recupererFloat

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,10 +1,23 @@
 saludar=function(){
     let nombreIngresado=recuperarTexto("txtNombre");
     let apellidoIngresado=recuperarTexto("txtApellido");
-    alert("Hola "+nombreIngresado+" "+apellidoIngresado);
+    let edadIngresada=recuperarInt("txtEdad");
+    let estaturaIngresada=recuperarFloat("txtEstatura");
+    console.log(nombreIngresado,"",apellidoIngresado," ",edadIngresada," ",estaturaIngresada);
 }
+
 recuperarTexto=function(idComponente){
     let componente=document.getElementById(idComponente);
     let valorIngresado=componente.value;
     return valorIngresado;
+}
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero;
+}
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFloat=parseFloat(valorCaja);
+    return valorFloat;
 }
