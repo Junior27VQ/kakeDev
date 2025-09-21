caa746d8a4004b73fe1b34f6b9d6355a1d90b0a0
Junior27VQ <favq1999@outlook.com>
2025-09-20 19:08:42 -0500

    funcion mostarTexto implementada

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,7 +3,14 @@ saludar=function(){
     let apellidoIngresado=recuperarTexto("txtApellido");
     let edadIngresada=recuperarInt("txtEdad");
     let estaturaIngresada=recuperarFloat("txtEstatura");
+    let mensaje="Hola "+nombreIngresado+" "+apellidoIngresado+" tienes "+edadIngresada+" años y mides "+estaturaIngresada+" mts";
+    mostrarTexto("lblResultado",mensaje);
     console.log(nombreIngresado,"",apellidoIngresado," ",edadIngresada," ",estaturaIngresada);
+}
+
+mostrarTexto=function(idComponente,mensaje){
+    let componente=document.getElementById(idComponente);
+    componente.innerText=mensaje;
 }
 
 recuperarTexto=function(idComponente){
