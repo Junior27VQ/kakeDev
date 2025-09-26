3942f295ca1a69355fc5d52fb7a266b987ecbeb7
Junior27VQ <favq1999@outlook.com>
2025-09-25 21:32:50 -0500

    ejercicio promedio de notas version2

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,10 +5,22 @@ calcularPromedioNotas=function(){
     let promedio=calcularPromedio(primero,segundo,tercero);
     let resultado=promedio.toFixed(2);
     let mensaje="El promedio es: "+resultado;
-    mostrarTexto("resultado",mensaje);
-    if(resultado>7){
-        mostrarImagen("imagen","./imagenes/36Tr.gif");
+    mostrarTexto("lblResultado",mensaje);
+    if(resultado>0 && resultado<5){
+        mostrarImagen("imagen","./imagenes/reprobado.gif");
+        let mensaje="REPROBADO";
+        mostrarTexto("lblmensaje",mensaje);
+    }else if(resultado>=5 && resultado<=8){
+        mostrarImagen("imagen","./imagenes/buenTrabajo.gif");
+        let mensaje=("BUEN TRABAJO");
+        mostrarTexto("lblmensaje",mensaje);
+    }else if(resultado>8 && resultado<=10){
+        mostrarImagen("imagen","./imagenes/excelente1.gif");
+        let mensaje="EXCELENTE";
+        mostrarTexto("lblmensaje",mensaje);
     }else{
-        mostrarImagen("imagen","./imagenes/soy-un-fracaso-total-finn.gif");
+        mostrarImagen("imagen","./imagenes/datosIncorrectos.gif");
+        let mensaje="DATODS INCORRECTOS";
+        mostrarTexto("lblmensaje",mensaje);
     }
 }
