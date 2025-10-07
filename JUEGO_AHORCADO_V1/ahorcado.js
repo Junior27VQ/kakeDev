7b66a4dd1856a8fe114abb318bd6f80f94277e8d
Junior27VQ <favq1999@outlook.com>
2025-10-06 22:25:22 -0500

    paso 1 completado

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,9 +1,30 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
-esMayuscula=function(caracter){
-    let cadena=caracter.charCodeAt();
-    if(cadena>=65 && cadena<=90){
+let palabraSecreta = "";
+esMayuscula = function (caracter) {
+    let cadena = caracter.charCodeAt();
+    if (cadena >= 65 && cadena <= 90) {
         return true;
-    }else{
+    } else {
         return false;
+    }
+}
+guardarPalabra = function () {
+    let palabra;
+    let esValido=true;
+    let caracteres = recuperarTexto("txtSecreta");
+    if (caracteres.length == 5) {
+
+        for (i = 0; i <= caracteres.length; i++) {
+            palabra = caracteres.charAt(i);
+            if (esMayuscula(palabra)) {
+                palabraSecreta = palabraSecreta + palabra;
+            } else if(!esMayuscula(palabra)){
+                esValido=false;
+            }
+        }
+    } if(esValido==false){
+        alert("debe ingresar una palabra de 5 letras mayusculas");
+    }else{
+        alert("debe ingresar una palabra de 5 letras mayusculas");
     }
 }
