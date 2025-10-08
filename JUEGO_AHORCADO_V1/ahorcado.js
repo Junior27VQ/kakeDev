d55c5c5726cff98a4a726e2a602725c006fec71c
Junior27VQ <favq1999@outlook.com>
2025-10-08 18:02:12 -0500

    paso 6 completado

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -59,6 +59,7 @@ validar=function(letra){
     if (correcto==false){
     alert("LA LETRA NO ES PARTE DE LA PALABRA")
     errores++;
+    mostrarAhorcado();
     }
 }
 ingresarLetra=function(){
@@ -74,4 +75,26 @@ ingresarLetra=function(){
     }else{
         alert("SOLO SE ACEPTAN LETRAS MAYUSCULAS");
     }
+}
+mostrarAhorcado=function(){
+    if(errores==1){
+        mostrarImagen("ahorcadoImagen","Ahorcado_01.png");
+    }else if(errores==2){
+        mostrarImagen("ahorcadoImagen","Ahorcado_02.png")
+    }else if(errores==3){
+        mostrarImagen("ahorcadoImagen","Ahorcado_03.png")
+    }else if(errores==4){
+        mostrarImagen("ahorcadoImagen","Ahorcado_04.png")
+    }else if(errores==5){
+        mostrarImagen("ahorcadoImagen","Ahorcado_05.png")
+    }else if(errores==6){
+        mostrarImagen("ahorcadoImagen","Ahorcado_06.png")
+    }else if(errores==7){
+        mostrarImagen("ahorcadoImagen","Ahorcado_07.png")
+    }else if(errores==8){
+        mostrarImagen("ahorcadoImagen","Ahorcado_08.png")
+    }else if(errores==9){
+        mostrarImagen("ahorcadoImagen","Ahorcado_09.png")
+    }
+
 }
