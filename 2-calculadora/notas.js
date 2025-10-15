45cd4195dbf1909dec7ad75a8c82fb1655b3b74c
Junior27VQ <favq1999@outlook.com>
2025-10-14 19:20:39 -0500

    archivo calculadora modificado

---
+++ 2-calculadora/notas.js
@@ -0,0 +1,108 @@
+calcularPromedio = function (nota1, nota2, nota3) {
+    let promedio;
+    promedio = (nota1 + nota2 + nota3) / 3;
+    return promedio;
+}
+
+calcular1 = function () {
+    let nota1;
+    let nota2;
+    let nota3;
+    let resultado;
+    let resultadoFormato;
+    nota1 = recuperarFloat("txtNota1");
+    if (isNaN(nota1)) {
+        alert("Por favor ingrese un numero valido en la nota 1");
+    } else {
+        nota2 = recuperarFloat("txtNota2");
+        if (isNaN(nota2)) {
+            alert("Por favor ingrese un numero valido en la nota 2");
+        } else {
+            nota3 = recuperarFloat("txtNota3");
+            if (isNaN(nota3)) {
+                alert("Por favor ingrese un numero valido en la nota 3");
+            } else {
+                resultado = calcularPromedio(nota1, nota2, nota3);
+                resultadoFormato = resultado.toFixed(2);
+                mostrarTexto("lblResultado", resultadoFormato);
+            }
+        }
+    }
+
+
+
+}
+calcular2 = function () {
+    let nota1;
+    let nota2;
+    let nota3;
+    let resultado;
+    let resultadoFormato;
+    let existeError = false;
+    nota1 = recuperarFloat("txtNota1");
+    if (isNaN(nota1)) {
+        mostrarTexto("lblError1", "Por favor ingrese un numero valido");
+        existeError = true;
+    } else {
+        mostrarTexto("lblError1", "");
+    }
+    nota2 = recuperarFloat("txtNota2");
+    if (isNaN(nota2)) {
+        mostrarTexto("lblError2", "Por favor ingrese un numero valido");
+        existeError = true;
+    } else {
+        mostrarTexto("lblError2", "");
+    }
+    nota3 = recuperarFloat("txtNota3");
+    if (isNaN(nota3)) {
+        mostrarTexto("lblError3", "Por favor ingrese un numero valido");
+        existeError = true;
+    } else {
+        mostrarTexto("lblError3", "");
+    }
+    if (existeError == false) {
+        resultado = calcularPromedio(nota1, nota2, nota3);
+        resultadoFormato = resultado.toFixed(2);
+        mostrarTexto("lblResultado", resultadoFormato);
+    }
+}
+calcular = function () {
+    let nota1;
+    let nota2;
+    let nota3;
+    let resultado;
+    let resultadoFormato;
+    let existeError = false;
+    nota1 = recuperarFloat("txtNota1");
+    nota2 = recuperarFloat("txtNota2");
+    nota3 = recuperarFloat("txtNota3");
+
+    if (esNotaValida(nota1, "lblError1") & esNotaValida(nota2, "lblError2") & esNotaValida(nota3, "lblError3")) {
+        resultado = calcularPromedio(nota1, nota2, nota3);
+        resultadoFormato = resultado.toFixed(2);
+        mostrarTexto("lblResultado", resultadoFormato);
+    } else {
+        mostrarTexto("lblResultado", "0.0");
+    }
+
+}
+esNotaValida = function (nota, idError) {
+    let hayErrores = false;
+    if (isNaN(nota)) {
+        mostrarTexto(idError, "Por favor ingrese un numero valido");
+        hayErrores = true;
+    }
+    if (nota < 0 || nota > 10) {
+        mostrarTexto(idError, "La nota debe estar entre 0 y 10");
+        hayErrores = true;
+    } 
+    if (hayErrores==false) {
+        mostrarTexto(idError, "");
+    }
+    return !hayErrores;
+}
+
+
+
+
+
