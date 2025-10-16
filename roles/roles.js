b85f81d007df2fad53179332cea5838eaa602fd7
Junior27VQ <favq1999@outlook.com>
2025-10-16 03:03:43 -0500

    guardar empleado funcionando correctamente

--- roles/roles.js
+++ roles/roles.js
@@ -1,20 +1,80 @@
 let empleados = [
-    {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 }
 ]
-esNuevo=false;
+let esNuevo = false;
 
-mostrarEmpleados=function(){
-    let cmpTabla=document.getElementById("tablaEmpleados");
-    let tabla="<table border='1'><tr><th>CEDULA</th><th>NOMBRE</th><th>APELLIDO</th><th>SUELDO</th></tr>";
-    for(let i=0;i<empleados.length;i++){
-        tabla+="<tr><td>"+empleados[i].cedula+"</td><td>"+empleados[i].nombre+"</td><td>"+empleados[i].apellido+"</td><td>"+empleados[i].sueldo+"</td></tr>";
+mostrarEmpleados = function () {
+    let cmpTabla = document.getElementById("tablaEmpleados");
+    let tabla = "<table border='1'><tr><th>CEDULA</th><th>NOMBRE</th><th>APELLIDO</th><th>SUELDO</th></tr>";
+    for (let i = 0; i < empleados.length; i++) {
+        tabla += "<tr><td>" + empleados[i].cedula + "</td><td>" + empleados[i].nombre + "</td><td>" + empleados[i].apellido + "</td><td>" + empleados[i].sueldo + "</td></tr>";
     }
-    tabla+="</table>";
-    cmpTabla.innerHTML=tabla;
+    tabla += "</table>";
+    cmpTabla.innerHTML = tabla;
+}
+guardar = function () {
+    let recuperarCedula = recuperarTexto("txtCedula");
+    let recuperarNombre = recuperarTexto("txtNombre");
+    let recuperarApellido = recuperarTexto("txtApellido");
+    let recuperarSueldo = recuperarFloat("txtSueldo");
+
+    if (recuperarCedula.length != 10) {
+        mostrarTexto("lblErrorCedula", "La cedula debe tener 10 caracteres");
+        esNuevo = false;
+    }
+    if (recuperarNombre.length > 3) {
+        mostrarTexto("lblErrorNombre", "El nombre debe tener maxim 3 caracteres");
+        esNuevo = false;
+    } else if (recuperarNombre != recuperarNombre.toUpperCase()) {
+            mostrarTexto("lblErrorNombre", "El nombre debe tener caracteres mayusculas");
+            esNuevo = false;
+    }
+    if (recuperarApellido != recuperarApellido.toUpperCase()) {
+        mostrarTexto("lblErrorApellido", "El apellido debe tener caracteres mayusculas");
+        esNuevo = false;
+    } else if (recuperarApellido.length > 3) {
+            mostrarTexto("lblErrorApellido", "El apellido debe tener maxim 3 caracteres");
+            esNuevo = false;
+    }
+    if (recuperarSueldo < 400 || recuperarSueldo > 500) {
+        mostrarTexto("lblErrorSueldo", "El sueldo debe ser mayor a 0");
+        esNuevo = false;
+    }
+    if (esNuevo == true) {
+        let empleado = { cedula: recuperarCedula, nombre: recuperarNombre, apellido: recuperarApellido, sueldo: recuperarSueldo };
+        let nuevo = agregarEmpleado(empleado);
+        if (nuevo == true) {
+        alert("EMPLEADO GUARDADO CORRECTAMENTE: ");
+        mostrarEmpleados();}else {
+        alert("YA EXISTE UN EMPLEADO CON LA CEDULA: " + empleado.cedula);
+       }
+    }
+}
+function agregarEmpleado(empleado) {
+    let resultado = buscarEmpleado(empleado.cedula);
+    if (resultado == null) {
+        empleados.push(empleado);
+        return true;
+    } else {
+        alert("El cliente ya existe");
+        return false;
+    }
+}
+function buscarEmpleado(cedula) {
+    let elementoEmpleado;
+    let empleadoEncontrado = null;
+    for (let i = 0; i < empleados.length; i++) {
+        elementoEmpleado = empleados[i];
+        if (elementoEmpleado.cedula == cedula) {
+            empleadoEncontrado = elementoEmpleado;
+            break;
+        }
+    }
+    return empleadoEncontrado;
 }
 
-mostrarOpcionEmpleados=function(){
+mostrarOpcionEmpleados = function () {
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
@@ -24,23 +84,55 @@ mostrarOpcionEmpleados=function(){
     deshabilitarComponente("txtApellido");
     deshabilitarComponente("txtSueldo");
     deshabilitarComponente("btnGuardar");
-    esNuevo=false;
+    esNuevo = false;
 }
-mostrarOpcionRoles=function(){
+mostrarOpcionRoles = function () {
     mostrarComponente("divRol");
     ocultarComponente("divEmpleado");
     ocultarComponente("divResumen");
 }
-mostrarOpcionResumen=function(){
+mostrarOpcionResumen = function () {
     mostrarComponente("divResumen");
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
 }
-ejecutarNuevo=function(){
+ejecutarNuevo = function () {
     habilitarComponente("txtCedula");
     habilitarComponente("txtNombre");
     habilitarComponente("txtApellido");
     habilitarComponente("txtSueldo");
     habilitarComponente("btnGuardar");
-    esNuevo=true;
+    esNuevo = true;
+}
+guardar1 = function () {
+    let recuperarCedula = recuperarTexto("txtCedula");
+    let recuperarNombre = recuperarTexto("txtNombre");
+    let recuperarApellido = recuperarTexto("txtApellido");
+    let recuperarSueldo = recuperarFloat("txtSueldo");
+    if (recuperarCedula.length != 10) {
+        mostrarTexto("lblErrorCedula", "La cedula debe tener 10 caracteres");
+        esNuevo = false;
+    }
+    if (recuperarNombre.length > 3) {
+        mostrarTexto("lblErrorNombre", "El nombre debe tener maxim 3 caracteres");
+        esNuevo = false;
+    }
+    if (recuperarApellido.length > 3) {
+        mostrarTexto("lblErrorApellido", "El apellido debe tener maxim 3 caracteres");
+        esNuevo = false;
+    }
+    if (recuperarSueldo < 400 || recuperarSueldo > 500) {
+        mostrarTexto("lblErrorSueldo", "El sueldo debe ser mayor a 0");
+        esNuevo = false;
+    }
+    if (esNuevo == true) {
+        let empleado = { cedula: recuperarCedula, nombre: recuperarNombre, apellido: recuperarApellido, sueldo: recuperarSueldo };
+        let nuevo = agregarEmpleado(empleado);
+
+        alert("Empleado guardado correctamente: " + nuevo);
+        mostrarEmpleados();
+
+    } else {
+        alert("LLA EXISTE UN EMPLEADO CON LA CEDULA: " + empleados.cedula);
+    }
 }
