77610c78f4f5cebbbfd3ff6f36d80fde60409b6b
Junior27VQ <favq1999@outlook.com>
2025-10-15 21:41:34 -0500

    funcion mostrarEmpleados implementada

--- roles/roles.js
+++ roles/roles.js
@@ -2,10 +2,22 @@ let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
     {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
 ]
+
+mostrarEmpleado=function(){
+    let cmpTabla=document.getElementById("tablaEmpleados");
+    let tabla="<table border='1'><tr><th>CEDULA</th><th>NOMBRE</th><th>APELLIDO</th><th>SUELDO</th></tr>";
+    for(let i=0;i<empleados.length;i++){
+        tabla+="<tr><td>"+empleados[i].cedula+"</td><td>"+empleados[i].nombre+"</td><td>"+empleados[i].apellido+"</td><td>"+empleados[i].sueldo+"</td></tr>";
+    }
+    tabla+="</table>";
+    cmpTabla.innerHTML=tabla;
+}
+
 mostrarOpcionEmpleados=function(){
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleado();
 }
 mostrarOpcionRoles=function(){
     mostrarComponente("divRol");
